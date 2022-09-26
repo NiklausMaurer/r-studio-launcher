8a68e61e6baedbdfa6e9968d1de345d5415c79a3
Niklaus Maurer <niklaus.maurer@rethinc.ch>
2022-09-26 21:30:19 +0100

    disables authentication

--- rstudio.sh
+++ rstudio.sh
@@ -12,7 +12,7 @@ function main() {
 	local container_id=$(run_container ${directory})
 	local container_port=$(get_container_port ${container_id})
 
-	open_prowser_when_ready ${container_port} &
+	open_browser_when_ready ${container_port} &
 
 	docker attach ${container_id}
 }
@@ -22,9 +22,10 @@ function run_container() {
 	local directory_name=$(basename ${directory})
 
 	docker run --rm -d \
-		-e PASSWORD=$(cat secrets/password) \
+		-e DISABLE_AUTH=true  \
 		-v ${directory}:/home/rstudio/${directory_name} \
-		-p 8787 rocker/rstudio
+		-p 127.0.0.1:0:8787 \
+		rocker/rstudio
 }
 
 function get_container_port() {
@@ -39,8 +40,9 @@ function get_container_port() {
 	echo ${container_port}
 }
 
-function open_prowser_when_ready() {
+function open_browser_when_ready() {
 	local container_port=${1}
+
 	until nc -z localhost ${container_port}  &> /dev/null; do sleep 1; done
 	xdg-open "http://localhost:${container_port}"
 }
